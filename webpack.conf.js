d84891c0e3ec74ee4a61fafbc4fa9277be1537a0
Miles Dahmann <mdahmann@gmail.com>
2017-05-24 02:11:06 -0500

    initial commit

---
+++ webpack.conf.js
@@ -0,0 +1,37 @@
+var webpack = require('webpack');
+
+module.exports = {
+    watch: false,
+    devtool: 'source-map',
+    context: __dirname,
+    entry: {
+        'theme-bundle': './assets/js/app.js',
+    },
+    output: {
+        path: `./assets/dist`,
+        filename: '[name].js',
+    },
+    bail: true,
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel',
+                include: /(assets\/js|assets\\js|stencil-utils)/,
+                query: {
+                    compact: false,
+                    cacheDirectory: true,
+                    presets: ['es2015-loose'],
+                }
+              }
+
+        ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery',
+        })
+    ],
+};
